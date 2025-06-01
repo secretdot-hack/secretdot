59584c5841ce4822502d11d907900da1f13d931b
Nicobustelo <nbustelo2002@gmail.com>
2025-06-01 18:23:49 -0300

    Redirect to LoginScreen added

--- secretdotfrontend/src/app/page.tsx
+++ secretdotfrontend/src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, [router]);
+
+  return null;
 }
