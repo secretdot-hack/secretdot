44ae94dc4d0cb23d0f3505ec45df55492e1fcd34
JuanJo Ciarlante <juanjosec@gmail.com>
2025-06-02 10:47:07 -0300

    few updates
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- smartcontract/02-get_testnet_tokens.sh
+++ smartcontract/02-get_testnet_tokens.sh
@@ -1,1 +1,3 @@
-echo Visit https://apps.moonbeam.network/moonbase-alpha/faucet/ and load tokens to the Metamask address
+#!/bin/bash
+source .env.public || exit 1
+echo Visit https://apps.moonbeam.network/moonbase-alpha/faucet/ and load tokens to the Metamask address: ${WALLET_ADDRESS:?}
